ba00aa4d20a06432564c99f85303b4e578d1b115
Kylian Patry <kylian.patry@lycee-faure.fr>
2023-11-07 15:20:32 +0100

    update

--- src/Controller/MesFichesController.php
+++ src/Controller/MesFichesController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\FicheFraisRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MesFichesController extends AbstractController
 {
     #[Route('/mesFiches', name: 'app_mes_fiches')]
-    public function index(): Response
+    public function index(Request $request, FicheFraisRepository $ficheFraisRepository): Response
     {
+        $theFiche = null;
+
+        if($request->get("dateFicheFrais") != null) {
+
+            $dateTime = new \DateTime($request->get("dateFicheFrais"));
+            $dateFormate = $dateTime->format('F Y');
+
+            $theFiche = $ficheFraisRepository->findOneBy(["mois"=>$dateFormate, "user"=>$this->getUser()]);
+        }
+
+
         return $this->render('mes_fiches/index.html.twig', [
             'controller_name' => 'MesFichesController',
+            'ficheDate' => $theFiche
         ]);
     }
 }
