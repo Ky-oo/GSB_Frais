3b132f21752f645ae504ddd7d581435a82368e28
Kylian Patry <kylian.patry@lycee-faure.fr>
2023-12-19 15:16:01 +0100

    Demande 2 fonctionnel

--- src/Controller/MontantTotalPourPrimeController.php
+++ src/Controller/MontantTotalPourPrimeController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\FicheFraisRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MontantTotalPourPrimeController extends AbstractController
 {
-    #[Route('/montant/total/pour/prime', name: 'app_montant_total_pour_prime')]
-    public function index(): Response
+    #[Route('/montant', name: 'app_montant_total_pour_prime')]
+    public function index(FicheFraisRepository $ficheFraisRepository, userRepository $userRepository): Response
     {
+        $montant = 0;
+        $prime = 0;
+
+     foreach ($ficheFraisRepository->findAll() as $fiche){
+         $anneeMois = $fiche->getMois();
+         $annee = substr($anneeMois, -4);
+         if($annee == 2023){
+             $montant += $fiche->getMontantValid();
+         }
+     }
+
+     $countUser = count($userRepository->findAll())-1; //-1 pour ne pas compter le compte administrateur
+        $prime = round(9.5 * $montant / 100, 2);
+        $primeParEmployee = round($prime / $countUser, 2);
         return $this->render('montant_total_pour_prime/index.html.twig', [
             'controller_name' => 'MontantTotalPourPrimeController',
+            'montant' => $montant,
+            'prime' => $prime,
+            'primeParEmployee' => $primeParEmployee
         ]);
     }
 }
