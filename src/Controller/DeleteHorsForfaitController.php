cdcee9c39df687642f6c55f7b31a4a3321e6f857
Kylian Patry <kylian.patry@lycee-faure.fr>
2024-03-19 13:50:08 +0100

    update

---
+++ src/Controller/DeleteHorsForfaitController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\LigneFraisHorsForfait;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DeleteHorsForfaitController extends AbstractController
+{
+    #[Route('/deleteHorsForfait/{id}', name: 'app_delete_hors_forfait')]
+    public function index($id, EntityManagerInterface $entityManager): Response
+    {
+        $fraisHorsForfait = $entityManager->getRepository(LigneFraisHorsForfait::class)->find($id);
+
+        if ($fraisHorsForfait) {
+            $entityManager->remove($fraisHorsForfait);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_saisie_fiche');
+    }
+}
